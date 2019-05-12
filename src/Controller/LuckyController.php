9647a709d14330156791c10acc6e17723d41f042
slik <stanislavdru@gmail.com>
2019-05-12 18:07:32 +0600

    my first page

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,8 +8,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
+  public function number()
+  {
+    $number = random_int(0, 100);
 
+    return new Response(
+      '<html><body>Lucky number: '.$number.'</body></html>'
+    );
+  }
 }
